000768f8d550e22857b5c1041a3e9aa7599ea7c9
Iman Kazerani <Kazeraniman@gmail.com>
2018-04-16 20:59:47 -0400

    Correctly constraining the pipe opening to the bounds of the screen.

--- Scenes/ObstacleSpawner/ObstacleSpawner.gd
+++ Scenes/ObstacleSpawner/ObstacleSpawner.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 signal score_point
 
-var PIPE_POSITION_RANGE = 440
+var IMMOVABLE_PIPE_GAP_SIZE = 220 # Size of pipe opening + two pipe heads with some leniency (206 is the true value)
+var GROUND_ALLOWANCE = 50 # Thickness of the ground texture with some leniency (49 is the true value)
+
+var pipe_opening_position_range
 
 var obstacle_scene
 
 func _ready():
+	# Determine the range of allowed values for the pipe opening position
+	var screen_size = get_viewport_rect().size
+	pipe_opening_position_range = int(round(screen_size.y - IMMOVABLE_PIPE_GAP_SIZE - GROUND_ALLOWANCE))
 	# Load the obstacle scene for further use
 	obstacle_scene = load("res://Scenes/Obstacle/Obstacle.tscn")
 
@@ -20,7 +26,7 @@ func _on_GenerationTimeout_timeout():
 	# Generate a new obstacle
 	var obstacle = obstacle_scene.instance()
 	# Position it at a random valid point
-	obstacle.position.y = randi() % PIPE_POSITION_RANGE - (PIPE_POSITION_RANGE / 2)
+	obstacle.position.y = pipe_opening_position_range + int(round(IMMOVABLE_PIPE_GAP_SIZE / 2))
 	# Set up the score signal forwarding
 	obstacle.connect("score_point", self, "forward_score_signal")
 	# Add it to the scene
