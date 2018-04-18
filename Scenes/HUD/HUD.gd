549f326aa0e3cd26b59e7328332f1e33f262e57a
Iman Kazerani <Kazeraniman@gmail.com>
2018-04-18 02:07:13 -0400

    Adding delay before the game may be restarted to prevent accidental restarts and adding in a shortcut to allow the fly buttons to be used to restart the game for ease of use.

--- Scenes/HUD/HUD.gd
+++ Scenes/HUD/HUD.gd
@@ -7,6 +7,11 @@ enum State {SETUP, PLAYING, MENU}
 func _ready():
 	show_mode(State.SETUP)
 
+func _process(delta):
+	# Use the flying buttons to restart the game if the menu is active and the accidental-click period has ended
+	if Input.is_action_just_released("fly") and $Menu.visible and not $Menu/CenterContainer/MenuBackground/CenterContainer/VBoxContainer/CenterContainer/TextureButton.disabled:
+		_on_TextureButton_pressed()
+
 func set_score_label(score):
 	# Set both of the score labels in step
 	$ScoreLabel.text = str(score)
